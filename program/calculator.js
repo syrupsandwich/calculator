dfdb7af881c9e897d1cd65d4bc098ef8dd5b74d9
sau <dontsendsamspam@gmail.com>
2022-06-27 14:06:41 -0700

    Create a script with the basic functions for calculator

---
+++ program/calculator.js
@@ -0,0 +1,27 @@
+function add(a, b){
+  return +a + +b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+//imput must be a string
+function operate(imput){
+  let operand = Array.from(`${imput}`).find((a)=>(a == '+' || a == '-' || a == '*' || a == '/'));
+  // console.log(`operand: ${operand}`);
+  let numbers = `${imput}`.split(`${operand}`);
+  // console.log(`numbers: ${numbers}`);
+  if (operand == '+') { return add(numbers[0], numbers[1])};
+  if (operand == '-') {return subtract(numbers[0], numbers[1])};
+  if (operand == '*') {return multiply(numbers[0], numbers[1])};
+  if (operand == '/') {return divide(numbers[0], numbers[1])};
+}
