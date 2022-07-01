1be7afc164e438e954a142e8df4aeae162e7da39
sau <dontsendsamspam@gmail.com>
2022-07-01 12:40:03 -0700

    Change the multiplication button symbol and fix typo
    Make the multiplication symbol look better aligned to be recognized as
    an operator.

--- program/calculator.js
+++ program/calculator.js
@@ -14,18 +14,15 @@ function divide(a, b){
   return a / b;
 }
 
-//imput must be a string
-function operate(imput){
-  let operand = Array.from(`${imput}`).find((a)=>(a == '+' || a == '-' || a == '*' || a == '/'));
-  // console.log(`operand: ${operand}`);
-  let numbers = `${imput}`.split(`${operand}`);
-  // console.log(`numbers: ${numbers}`);
+//input must be a string
+function operate(input){
+  let operand = Array.from(`${input}`).find((a)=>(a == '+' || a == '-' || a == '&#215;' || a == '/'));
+  let numbers = `${input}`.split(`${operand}`);
   if (operand == '+') { return add(numbers[0], numbers[1])};
   if (operand == '-') {return subtract(numbers[0], numbers[1])};
-  if (operand == '*') {return multiply(numbers[0], numbers[1])};
+  if (operand == '&#215;') {return multiply(numbers[0], numbers[1])};
   if (operand == '/') {return divide(numbers[0], numbers[1])};
 }
-
 
 let results = document.querySelector('#results');
 let buttons = document.querySelector('.body');
